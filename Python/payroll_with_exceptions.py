dc8eabe563aecde93e3ace252e0c822fe77a0444
Marcos Gutierrez <victordavidmarcos123@gmail.com>
2026-02-05 11:29:45 -0800

    second commit

---
+++ Python/payroll_with_exceptions.py
@@ -0,0 +1,15 @@
+# payroll_with_exceptions.py
+try:
+    hours = float(input("Enter hours worked: "))
+    rate = float(input("Enter hourly rate: "))
+
+    if hours > 40:
+        overtime = hours - 40
+        pay = 40 * rate + overtime * rate * 1.5
+    else:
+        pay = hours * rate
+
+    print("Pay:", pay)
+
+except ValueError:
+    print("Error: Please enter numeric input only.")
