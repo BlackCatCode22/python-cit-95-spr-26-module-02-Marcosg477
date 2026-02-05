47bf443af48e4eb6a5244054ac5cf4cdd33f571a
Marcos Gutierrez <victordavidmarcos123@gmail.com>
2026-02-05 11:28:06 -0800

    first commit

---
+++ Python/payroll.py
@@ -0,0 +1,11 @@
+# payroll.py
+hours = float(input("Enter hours worked: "))
+rate = float(input("Enter hourly rate: "))
+
+if hours > 40:
+    overtime = hours - 40
+    pay = 40 * rate + overtime * rate * 1.5
+else:
+    pay = hours * rate
+
+print("Pay:", pay)
